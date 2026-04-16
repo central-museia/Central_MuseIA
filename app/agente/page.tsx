723771fdcd14c089b4f16b0ba05322de9f0c9c03
central-museia <agents.museia@gmail.com>
2026-04-16 12:29:02 -0300

    Add files via upload

---
+++ app/agente/page.tsx
@@ -0,0 +1,3 @@
+export default function AgentePage() {
+  return <main className="min-h-screen" />;
+}
---
+++ app/pagamento/page.tsx
@@ -0,0 +1,3 @@
+export default function PagamentoPage() {
+  return <main className="min-h-screen" />;
+}
