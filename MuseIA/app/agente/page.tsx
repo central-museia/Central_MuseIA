eb51fa43a1d658972bae60ea987c2fc4bd187317
central-museia <agents.museia@gmail.com>
2026-04-16 12:29:37 -0300

    Delete MuseIA/app directory

--- MuseIA/app/agente/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function AgentePage() {
-  return <main className="min-h-screen" />;
-}
--- MuseIA/app/pagamento/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function PagamentoPage() {
-  return <main className="min-h-screen" />;
-}
