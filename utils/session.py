91246cab0525bff74b85e2f6429635af3ead2859
central-museia <agents.museia@gmail.com>
2026-04-22 14:40:11 -0300

    Update session.py

--- utils/session.py
+++ utils/session.py
@@ -1,1 +1,15 @@
 
+def reset_agente_session():
+    import streamlit as st
+    
+    keys = [
+        "processamento_liberado",
+        "resultado_gerado",
+        "executar_agora",
+        "input_execucao",
+        "resultado_final",
+        "agente_selecionado"
+    ]
+    
+    for k in keys:
+        st.session_state[k] = False if "liberado" in k or "gerado" in k or "executar" in k else None
